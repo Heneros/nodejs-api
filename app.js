df7827ce88d759bdc7a99921af0265d59616fc33
Heneros <punk60826@gmail.com>
2023-10-02 21:34:41 +0300

    jobs controllers and auth.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const authenticateUser = require('./middleware/authentication');
+
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 ///connectDB
@@ -22,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -30,7 +32,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try { 
+  try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
