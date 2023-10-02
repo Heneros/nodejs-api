c81c1540242165cd062be77abe6dbb9509e9b84c
Heneros <punk60826@gmail.com>
2023-10-02 15:23:37 +0300

    controllers and models been updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,43 @@
 const { StatusCodes } = require("http-status-codes")
 
-const bcrypt  = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 const User = require("../models/User")
 
 const register = async (req, res) => {
     try {
-        const { name, email, password } = req.body;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const tempUser = { name, email, password: hashedPassword };
+        // const { name, email, password } = req.body;
+        // const salt = await bcrypt.genSalt(10);
+        // const hashedPassword = await bcrypt.hash(password, salt);
+        // const tempUser = { name, email, password: hashedPassword };
 
-        const user = await User.create({ ...tempUser })
-        res.status(StatusCodes.CREATED).json({ user })
+        const user = await User.create({ ...req.body })
+        const token = user.createJWT();
+        res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
     } catch (error) {
         console.log(error)
     }
 
 }
 const login = async (req, res) => {
-    res.send('login user')
+    // try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+    // } catch (error) {
+    //     console.log(error)
+    // }
 }
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -19,6 +22,22 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Please provide password'],
         minlength: 6
     }
-})
+});
+
+
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    // next();
+});
+/// Methods это instance
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+}
+
+
 
 module.exports = mongoose.model('User', UserSchema); 
